ece9400f90e0f1cc4d39e533d69bd8cb5016ef2e
Mark Norris <mdnorrisde@gmail.com>
2021-12-03 12:09:33 +0100

    Update day1_part1.py

--- day1/day1_part1.py
+++ day1/day1_part1.py
@@ -6,12 +6,12 @@ def larger_than_previous():
         list_of_nums = []
         line = fp.readline()
         while line:
-            list = list + [int(line)]
+            list_of_nums = list_of_nums + [int(line)]
             line = fp.readline()
 
     larger_num_count = 0
-    for i in range(len(list) - 1):
-        if list[i] < list[i+1]:
+    for i in range(len(list_of_nums) - 1):
+        if list_of_nums[i] < list_of_nums[i+1]:
             larger_num_count +=1
 
     return larger_num_count
