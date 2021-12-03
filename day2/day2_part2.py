a0ebd2c8f278a11b5dc5e3cdebdd90983cfffa44
Mark Norris <mdnorrisde@gmail.com>
2021-12-03 11:19:25 +0100

    completed day2_part2.py

--- day2/day2_part2.py
+++ day2/day2_part2.py
@@ -1,21 +1,18 @@
 def dive_with_aim():
     with open(r'C:\Users\Emilio\Documents\GitHub\advent_of_code2021\day2\day2_puzzle_input.txt') as fp:
         line = fp.readline()
+        string = "" # empty string to store submarine commands
         horizontal_position = 0
         depth = 0
         aim = 0
-        string = ""
-        x = 0
         while line:
             for i in range(len(line)):
-                if ord(line[i]) != 32:
+                if ord(line[i]) != 32:  # If the character is not equal to " ", stores the character in the string variable 
                     string = string + line[i]
-                else:
+                else:   # Once the command word is complete, determines the values for each variable
                     value = int(line[i+1])
                     if string == 'forward':
                         horizontal_position = horizontal_position + value
-                        print(f"Horizontal position{x} = {horizontal_position}")
-                        x+=1
                         if aim > 0:
                             depth = depth + (value * aim)
                     elif string == 'up':
@@ -24,9 +21,9 @@ def dive_with_aim():
                             aim = 0
                     else:
                         aim = aim + int(line[i+1])
+                    string = ""
                     break
             line = fp.readline()
-    print(f"Horizontal position = {horizontal_position}\ndepth = {depth}")
     return horizontal_position*depth
 
 def main():
